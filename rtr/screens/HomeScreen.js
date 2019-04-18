f72ae14f9fe7af9986dcac1470666946b63d7772
ashley nguyen <ashleygnguyen@yahoo.com>
2019-04-18 08:41:51 -0500

    keyboard view, styling changes

--- rtr/screens/HomeScreen.js
+++ rtr/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Image, ImageBackground, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {height, width} from '../constants/Layout';
-import {Button} from 'react-native-elements';
+import { Image, ImageBackground, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { height, width } from '../constants/Layout';
+import { Button } from 'react-native-elements';
 import homescreen from '../assets/images/homescreen.jpeg';
 import colors from '../constants/Colors';
 import Icon from 'react-native-vector-icons/Feather';
@@ -16,24 +16,64 @@ import today from '../assets/images/login-background.jpg';
 import Footer from '../components/Footer/Footer';
 
 export default class HomeScreen extends React.Component {
-  state = {screen: 'Home'};
-  static navigationOptions = {
-    header: null,
-  };
+	state = { screen: 'Home' };
+	static navigationOptions = {
+		header: null,
+	};
 
-  componentDidMount() {
-    this.animation.play(0, 164);
-  }
+	componentDidMount() {
+		this.animation.play(0, 164);
+	}
 
-<<<<<<< HEAD
+	route(path) {
+		this.props.navigation.navigate(path);
+	}
+
+	render() {
+		const { screen } = this.state;
+		const icon1 = <Icon name='linkedin' size={60} color='white' />;
+		const play = (
+			<Animation
+				ref={animation => {
+					this.animation = animation;
+				}}
+				// style={{height: 200, width}}
+				loop={false}
+				source={title}
+			/>
+		);
+		const todaysRosary = (
+			<Text>
+				"Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a
+				little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters
+				in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an
+				infant named Jeb." - Samuel L. 31:17
+      </Text>
+		);
+
 		return (
-			<ImageBackground source={homescreen} style={{ flex: 1, opacity: .85 }}>
-				<Text style={styles.title}>Run The Rosary</Text>
+			<ImageBackground source={homescreen} style={{ flex: 1, opacity: 0.85 }}>
+				<View style={styles.headerContainer}>
+					<TouchableOpacity
+						style={{ paddingLeft: 30 }}
+						onPress={() => {
+							this.route('Home');
+						}}>
+						<Icon name='menu' size={30} color='#fff' />
+					</TouchableOpacity>
+					<Text>Run the Rosary</Text>
+					<TouchableOpacity
+						style={{ paddingRight: 30 }}
+						onPress={() => {
+							this.route('User');
+						}}>
+						<Icon2 name='user' size={30} color='#fff' />
+					</TouchableOpacity>
+				</View>
 				<ScrollView>
 					<View
 						style={{
 							flex: 1,
-							flexDirection: 'column',
 							alignContent: 'center',
 							justifyContent: 'center',
 						}}>
@@ -41,250 +81,127 @@ export default class HomeScreen extends React.Component {
 							style={{
 								flex: 1,
 								flexDirection: 'row',
+								flexWrap: 'wrap',
 								alignContent: 'center',
 								justifyContent: 'center',
 							}}>
 							<Button
-								title={icon1}
-								onPress={() => this.props.navigation.navigate('RosaryList')}
-								buttonStyle={{
-									opacity: .8,
-									backgroundColor: colors.blue,
-									marginVertical: height / 20,
-									marginHorizontal: 5,
-									height: height / 5,
-									width: height / 5,
-								}}
+								title={todaysRosary}
+								titleStyle={styles.todayText}
+								onPress={() => this.route('RosaryList')}
+								buttonStyle={styles.todayButton}
+							/>
+							<Button
+								icon={play}
+								onPress={() => this.route('PrayerPlayer')}
+								buttonStyle={[styles.playButton, { backgroundColor: 'transparent' }]}
+							/>
+							<Button
+								title='Rosary'
+								titleStyle={styles.buttonText}
+								icon={<Image source={rosary} style={{ height: 100, width: 100 }} />}
+								onPress={() => this.route('RosaryList')}
+								buttonStyle={styles.prayerButton}
 							/>
 							<Button
 								title='Prayer'
-								onPress={() => this.props.navigation.navigate('PrayerList')}
-								buttonStyle={{
-									opacity: .8,
-									backgroundColor: colors.blue,
-									marginVertical: height / 20,
-									marginHorizontal: 5,
-									height: height / 5,
-									width: height / 5,
-								}}
-							/>
-						</View>
-						<View
-							style={{
-								flex: 1,
-								flexDirection: 'row',
-								alignContent: 'center',
-								justifyContent: 'center',
-							}}>
-							<Button
-								title={icon1}
-								onPress={() => this.props.navigation.navigate('RosaryList')}
-								buttonStyle={{
-									opacity: .8,
-									backgroundColor: colors.blue,
-									marginVertical: height / 18,
-									marginHorizontal: 5,
-									height: height / 5,
-									width: height / 5,
-								}}
-							/>
-							<Button
-								title='Prayer'
-								onPress={() => this.props.navigation.navigate('PrayerList')}
-								buttonStyle={{
-									opacity: .8,
-									backgroundColor: colors.blue,
-									marginVertical: height / 18,
-									marginHorizontal: 5,
-									height: height / 5,
-									width: height / 5,
-								}}
+								titleStyle={styles.buttonText}
+								icon={<Image source={pray} style={{ height: 100, width: 100 }} />}
+								onPress={() => this.route('PrayerList')}
+								buttonStyle={styles.prayerButton}
 							/>
 						</View>
 					</View>
 				</ScrollView>
+				<View style={styles.footerContainer}>
+					<TouchableOpacity
+						onPress={() => {
+							this.route('Splash');
+						}}>
+						<Icon2 name='home' size={30} color='#fff' />
+					</TouchableOpacity>
+					<TouchableOpacity
+						onPress={() => {
+							this.route('RosaryList');
+						}}>
+						<Icon2 name='book' size={30} color='#fff' />
+					</TouchableOpacity>
+					<TouchableOpacity
+						onPress={() => {
+							this.route('PrayerList');
+						}}>
+						<Icon2 name='plus' size={30} color='#fff' />
+					</TouchableOpacity>
+					<TouchableOpacity
+						onPress={() => {
+							this.route('PrayerPlayer');
+						}}>
+						<Icon2 name='play' size={30} color='#fff' />
+					</TouchableOpacity>
+				</View>
 			</ImageBackground>
 		);
 	}
-=======
-  route(path) {
-    this.props.navigation.navigate(path);
-  }
-
-  render() {
-    const {screen} = this.state;
-    const icon1 = <Icon name='linkedin' size={60} color='white' />;
-    const play = (
-      <Animation
-        ref={animation => {
-          this.animation = animation;
-        }}
-        // style={{height: 200, width}}
-        loop={false}
-        source={title}
-      />
-    );
-    const todaysRosary = (
-      <Text>
-        "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a
-        little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters
-        in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an
-        infant named Jeb." - Samuel L. 31:17
-      </Text>
-    );
-
-    return (
-      <ImageBackground source={homescreen} style={{flex: 1, opacity: 0.85}}>
-        <View style={styles.headerContainer}>
-          <TouchableOpacity
-            style={{paddingLeft: 30}}
-            onPress={() => {
-              this.route('Home');
-            }}>
-            <Icon name='menu' size={30} color='#fff' />
-          </TouchableOpacity>
-          <Text>Run the Rosary</Text>
-          <TouchableOpacity
-            style={{paddingRight: 30}}
-            onPress={() => {
-              this.route('User');
-            }}>
-            <Icon2 name='user' size={30} color='#fff' />
-          </TouchableOpacity>
-        </View>
-        <ScrollView>
-          <View
-            style={{
-              flex: 1,
-              alignContent: 'center',
-              justifyContent: 'center',
-            }}>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'row',
-                flexWrap: 'wrap',
-                alignContent: 'center',
-                justifyContent: 'center',
-              }}>
-              <Button
-                title={todaysRosary}
-                titleStyle={styles.todayText}
-                onPress={() => this.route('RosaryList')}
-                buttonStyle={styles.todayButton}
-              />
-              <Button
-                icon={play}
-                onPress={() => this.route('PrayerPlayer')}
-                buttonStyle={[styles.playButton, {backgroundColor: 'transparent'}]}
-              />
-              <Button
-                title='Rosary'
-                titleStyle={styles.buttonText}
-                icon={<Image source={rosary} style={{height: 100, width: 100}} />}
-                onPress={() => this.route('RosaryList')}
-                buttonStyle={styles.prayerButton}
-              />
-              <Button
-                title='Prayer'
-                titleStyle={styles.buttonText}
-                icon={<Image source={pray} style={{height: 100, width: 100}} />}
-                onPress={() => this.route('PrayerList')}
-                buttonStyle={styles.prayerButton}
-              />
-            </View>
-          </View>
-        </ScrollView>
-        <View style={styles.footerContainer}>
-          <TouchableOpacity
-            onPress={() => {
-              this.route('Splash');
-            }}>
-            <Icon2 name='home' size={30} color='#fff' />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.route('RosaryList');
-            }}>
-            <Icon2 name='book' size={30} color='#fff' />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.route('PrayerList');
-            }}>
-            <Icon2 name='plus' size={30} color='#fff' />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.route('PrayerPlayer');
-            }}>
-            <Icon2 name='play' size={30} color='#fff' />
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
-    );
-  }
->>>>>>> master
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  headerContainer: {
-    backgroundColor: 'black',
-    width,
-    height: height / 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingTop: 35,
-  },
-  home: {
-    margin: 20,
-  },
-  playButton: {
-    backgroundColor: 'transparent',
-    borderRadius: 5,
-    borderColor: colors.blue,
-    borderWidth: 2,
-    height: 230,
-    width: 270,
-    marginVertical: 30,
-    marginHorizontal: 30,
-  },
-  prayerButton: {
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-    opacity: 0.9,
-    backgroundColor: colors.blue,
-    height: 225,
-    width: 180,
-    margin: 10,
-    borderRadius: 5,
-  },
-  todayButton: {
-    opacity: 0.6,
-    backgroundColor: colors.darkgray,
-    height: 150,
-    width: 375,
-    marginVertical: 20,
-  },
-  buttonText: {
-    fontWeight: 'bold',
-  },
-  todayText: {
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-  footerContainer: {
-    backgroundColor: colors.darkgray,
-    opacity: 0.8,
-    width,
-    height: height / 13,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 50,
-  },
+	container: {
+		flex: 1,
+	},
+	headerContainer: {
+		backgroundColor: 'black',
+		width,
+		height: height / 10,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		paddingTop: 35,
+	},
+	home: {
+		margin: 20,
+	},
+	playButton: {
+		backgroundColor: 'transparent',
+		borderRadius: 5,
+		borderColor: colors.blue,
+		borderWidth: 2,
+		height: 230,
+		width: 270,
+		marginVertical: 30,
+		marginHorizontal: 30,
+	},
+	prayerButton: {
+		flexDirection: 'column',
+		justifyContent: 'space-evenly',
+		opacity: 0.9,
+		backgroundColor: colors.blue,
+		height: 225,
+		width: 180,
+		margin: 10,
+		borderRadius: 5,
+	},
+	todayButton: {
+		opacity: 0.6,
+		backgroundColor: colors.darkgray,
+		height: 150,
+		width: 375,
+		marginVertical: 20,
+	},
+	buttonText: {
+		fontWeight: 'bold',
+	},
+	todayText: {
+		fontSize: 12,
+		fontWeight: 'bold',
+	},
+	footerContainer: {
+		backgroundColor: colors.darkgray,
+		opacity: 0.8,
+		width,
+		height: height / 13,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		paddingHorizontal: 50,
+	},
 });
--- rtr/screens/Rosary/RosaryList.js
+++ rtr/screens/Rosary/RosaryList.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {StyleSheet, View, Text, ScrollView, ImageBackground, TouchableOpacity} from 'react-native';
-import {Button} from 'react-native-elements';
-import {width, height} from '../../constants/Layout';
+import { StyleSheet, View, Text, ScrollView, ImageBackground, TouchableOpacity } from 'react-native';
+import { Button } from 'react-native-elements';
+import { width, height } from '../../constants/Layout';
 import colors from '../../constants/Colors';
 import Icon from 'react-native-vector-icons/AntDesign';
-import background from '../../assets/images/login-background.jpg';
-import {connect} from 'react-redux';
-import {getPrayersByType, selectPrayer} from '../../ducks/reducers/prayerReducer';
+import rosarylist from '../../assets/images/rosarylist.jpg';
+import { connect } from 'react-redux';
+import { getPrayersByType, selectPrayer } from '../../ducks/reducers/prayerReducer';
 
 class RosaryList extends React.Component {
   state = {
@@ -25,18 +25,18 @@ class RosaryList extends React.Component {
   };
 
   done = () => {
-    let {prayers} = this.props;
+    let { prayers } = this.props;
     let selected = [];
     for (var i = 0; i < prayers.length; i++) {
       if (prayers[i].selected) {
         selected.push(prayers[i]);
       }
     }
-    this.setState({selectedPrayerList: selected});
+    this.setState({ selectedPrayerList: selected });
   };
 
   setPlayButton = () => {
-    let {prayers} = this.props;
+    let { prayers } = this.props;
 
     for (var i = 0; i < prayers.length; i++) {
       if (prayers.some(e => e.selected)) {
@@ -50,42 +50,42 @@ class RosaryList extends React.Component {
   }
 
   render() {
-    const {prayers, prayersLoading} = this.props;
+    const { prayers, prayersLoading } = this.props;
     console.log('Rosaries', this.props.prayers);
 
     const list = !prayersLoading
       ? prayers.map((e, i) => {
-          return (
+        return (
+          <TouchableOpacity
+            key={i}
+            style={styles.listContainer}
+            onPress={() => {
+              this.props.selectPrayer(e, i);
+            }}>
             <TouchableOpacity
-              key={i}
-              style={styles.listContainer}
               onPress={() => {
                 this.props.selectPrayer(e, i);
               }}>
-              <TouchableOpacity
-                onPress={() => {
-                  this.props.selectPrayer(e, i);
-                }}>
-                <Text style={e.selected ? styles.selectedListText : styles.unselectedListText}>{e.prayer_name}</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  this.props.selectPrayer(e, i);
-                }}>
-                {e.selected ? <Icon name='check' size={20} color='green' /> : <Icon name='plus' size={20} color='white' />}
-              </TouchableOpacity>
+              <Text style={e.selected ? styles.selectedListText : styles.unselectedListText}>{e.prayer_name}</Text>
             </TouchableOpacity>
-          );
-        })
+            <TouchableOpacity
+              onPress={() => {
+                this.props.selectPrayer(e, i);
+              }}>
+              {e.selected ? <Icon name='check' size={20} color='green' /> : <Icon name='plus' size={20} color='white' />}
+            </TouchableOpacity>
+          </TouchableOpacity>
+        );
+      })
       : null;
 
     return (
-      <ImageBackground style={styles.container} source={background}>
+      <ImageBackground style={[{ opacity: .6 }, styles.container]} source={rosarylist}>
         <View style={styles.header}>
           <TouchableOpacity onPress={() => this.props.navigation.navigate('Home')}>
             <Icon name='arrowleft' size={30} color='#fff' />
           </TouchableOpacity>
-          <Text style={[styles.text, {marginLeft: 70}]}>Select Rosary</Text>
+          <Text style={[styles.text, { marginLeft: 70 }]}>Select Rosary</Text>
         </View>
         <ScrollView contentContainerStyle={styles.scrollContainer}>{list}</ScrollView>
         <View styles={styles.footer}>
@@ -209,5 +209,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  {getPrayersByType, selectPrayer},
+  { getPrayersByType, selectPrayer },
 )(RosaryList);
